32c4cebb4b3bdd5f3ac37eeaf5a07af7680ab05d
JanetMutua <janetmutua04@gmail.com>
2021-04-24 15:39:20 +0300

    updated module

--- StudentModule.py
+++ StudentModule.py
@@ -1,11 +1,29 @@
-name_var = input('Enter fullname:')
-email_var = input('Enter email address:')
-contact_var = input('Enter phone number:')
-cert_var = input('Upload high school transcript:')
+
+# stud_info = open('studentfiles.txt', 'a')
+
+
+class Student:
+    def __init__(self, first_name,second_name,dob,adress):
+        self.first_name=first_name
+        self.second_name=second_name
+        self.dob=dob
+        self.address=adress
+
+    def get_profile(self):
+        print(self.first_name, self.second_name, self.dob)
 
 
 def reg_process():
-    print('Registration successful!')
+    name_var = input('Enter fullname:')
+    email_var = input('Enter email address:')
+    contact_var = input('Enter phone number:')
+    cert_var = input('Upload high school transcript:')
     get_input = [name_var, email_var, contact_var, cert_var]
-    for detail in get_input:
-        return(detail)
+    print('Registration successful!')
+    #for detail in get_input:
+        #return detail
+
+
+
+
+
