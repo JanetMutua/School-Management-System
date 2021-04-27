bee74eddfd7ca490cb3334ded2084e150ae6b0c0
JanetMutua <janetmutua04@gmail.com>
2021-04-27 11:14:38 +0300

    updated

--- CourseManagementModule.py
+++ CourseManagementModule.py
@@ -1,30 +1,24 @@
-class Course:
 
-    list_of_course = ["Stats","Bcom","Maths"]
-
-    def __init__(self):
-        self.unit_name=''
-
-    def getCourseOptions(self):
-        options = 'Menu:\n1. Unit Registration \n2. Course Audit \n3. Academic Transcript '
-        print(options)
-        our_input = input('Enter Selection:')
-        if our_input == str(1):
-            self.register_course()
-        if our_input == 2:
-            print('Check my grades')
-        if our_input == 3:
-            print('Download Transcript')
-
-    def register_course(self):
-        options = 'Select course no. to register'
-        counter = 1
-        for unit in Course.list_of_course:
-            print(counter, " : ",unit)
-            counter = counter + 1
+def course_dashboard():
+    dashboard = 'Main menu:\n1. Register courses\n2. Academics\n3.Course modules '
+    print(dashboard)
+    dashboard_entry = input('Select option:')
+    if dashboard_entry == '1':
+        print('Register courses')
+    elif dashboard_entry == '2':
+        print(academics_dash())
+    elif dashboard_entry == '3':
+        print('Download course modules!')
+    else:
+        pass
 
 
-
-
-
+def academics_dash():
+    my_dashboard = '1. Download Transcript\n2. View my grades'
+    print(my_dashboard)
+    my_dashboard_entry = input('Select option:')
+    if my_dashboard_entry == '1':
+        print('Downloading...')
+    else:
+        print('opening form...')
 
