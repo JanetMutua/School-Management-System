c39a104da3f20de6b9ceed636f26f369e3a5985e
JanetMutua <janetmutua04@gmail.com>
2021-04-27 11:15:46 +0300

    updated

--- StudentPortalModule.py
+++ StudentPortalModule.py
@@ -1,6 +1,8 @@
+
+import FeeManagementModule
 import CourseManagementModule
 import HostelManagementModule
-import FeeManagementModule
+
 
 def student_login():
     username_input = input('Enter username:')
@@ -10,14 +12,14 @@ def student_login():
             while 1:
                 print('Menu\n1. Course Management\n2. Finance\n3. Hostel Management\n4. Logout')
                 student_menu = input('Select Option:')
-                if student_menu == 1:
-                    course_menu = CourseManagementModule.Course
+                if student_menu == '1':
+                    course_menu = CourseManagementModule.course_dashboard()
                     print(course_menu)
-                elif student_menu == 2:
-                    fee_menu = FeeManagementModule
+                elif student_menu == '2':
+                    fee_menu = FeeManagementModule.finance_dashboard()
                     print(fee_menu)
-                elif student_menu == 3:
-                    hostels_menu = HostelManagementModule
+                elif student_menu == '3':
+                    hostels_menu = HostelManagementModule.hostel_dashboard()
                     print(hostels_menu)
                 else:
                     print('Not a valid option')
@@ -26,6 +28,3 @@ def student_login():
             print('Invalid Password')
     else:
         print('Invalid Login Credentials')
-
-
-
